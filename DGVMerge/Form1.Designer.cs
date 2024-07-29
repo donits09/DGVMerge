dee3fc344a1347e9c5a6852f150388b043fcf0b1
donits09 <98079068+donits09@users.noreply.github.com>
2024-07-29 09:59:47 +0800

    Add files via upload

---
+++ DGVMerge/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace DGVMerge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DGVUsers = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.password = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.last_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.first_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.middle_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.birthdate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contact_no = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.user_level = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVUsers
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGVUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.DGVUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVUsers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.username,
+            this.password,
+            this.last_name,
+            this.first_name,
+            this.middle_name,
+            this.birthdate,
+            this.address,
+            this.age,
+            this.contact_no,
+            this.status,
+            this.user_level});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DGVUsers.DefaultCellStyle = dataGridViewCellStyle2;
+            this.DGVUsers.Location = new System.Drawing.Point(27, 35);
+            this.DGVUsers.Name = "DGVUsers";
+            this.DGVUsers.RowHeadersWidth = 51;
+            this.DGVUsers.RowTemplate.Height = 24;
+            this.DGVUsers.Size = new System.Drawing.Size(1399, 380);
+            this.DGVUsers.TabIndex = 0;
+            this.DGVUsers.Paint += new System.Windows.Forms.PaintEventHandler(this.DGVUsers_Paint);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "ID";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.Width = 125;
+            // 
+            // username
+            // 
+            this.username.DataPropertyName = "username";
+            this.username.HeaderText = "USERNAME";
+            this.username.MinimumWidth = 6;
+            this.username.Name = "username";
+            this.username.Visible = false;
+            this.username.Width = 125;
+            // 
+            // password
+            // 
+            this.password.DataPropertyName = "password";
+            this.password.HeaderText = "PASSWORD";
+            this.password.MinimumWidth = 6;
+            this.password.Name = "password";
+            this.password.Visible = false;
+            this.password.Width = 125;
+            // 
+            // last_name
+            // 
+            this.last_name.DataPropertyName = "last_name";
+            this.last_name.HeaderText = "LAST NAME";
+            this.last_name.MinimumWidth = 6;
+            this.last_name.Name = "last_name";
+            this.last_name.Width = 125;
+            // 
+            // first_name
+            // 
+            this.first_name.DataPropertyName = "first_name";
+            this.first_name.HeaderText = "FIRST NAME";
+            this.first_name.MinimumWidth = 6;
+            this.first_name.Name = "first_name";
+            this.first_name.Width = 125;
+            // 
+            // middle_name
+            // 
+            this.middle_name.DataPropertyName = "middle_name";
+            this.middle_name.HeaderText = "MIDDLE NAME";
+            this.middle_name.MinimumWidth = 6;
+            this.middle_name.Name = "middle_name";
+            this.middle_name.Width = 125;
+            // 
+            // birthdate
+            // 
+            this.birthdate.DataPropertyName = "birthdate";
+            this.birthdate.HeaderText = "BIRTHDATE";
+            this.birthdate.MinimumWidth = 6;
+            this.birthdate.Name = "birthdate";
+            this.birthdate.Width = 125;
+            // 
+            // address
+            // 
+            this.address.DataPropertyName = "address";
+            this.address.HeaderText = "ADDRESS";
+            this.address.MinimumWidth = 6;
+            this.address.Name = "address";
+            this.address.Width = 125;
+            // 
+            // age
+            // 
+            this.age.DataPropertyName = "age";
+            this.age.HeaderText = "AGE";
+            this.age.MinimumWidth = 6;
+            this.age.Name = "age";
+            this.age.Width = 125;
+            // 
+            // contact_no
+            // 
+            this.contact_no.DataPropertyName = "contact_no";
+            this.contact_no.HeaderText = "CONTACT #";
+            this.contact_no.MinimumWidth = 6;
+            this.contact_no.Name = "contact_no";
+            this.contact_no.Width = 125;
+            // 
+            // status
+            // 
+            this.status.DataPropertyName = "status";
+            this.status.HeaderText = "STATUS";
+            this.status.MinimumWidth = 6;
+            this.status.Name = "status";
+            this.status.Visible = false;
+            this.status.Width = 125;
+            // 
+            // user_level
+            // 
+            this.user_level.DataPropertyName = "user_level";
+            this.user_level.HeaderText = "LEVEL";
+            this.user_level.MinimumWidth = 6;
+            this.user_level.Name = "user_level";
+            this.user_level.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1460, 446);
+            this.Controls.Add(this.DGVUsers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVUsers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn password;
+        private System.Windows.Forms.DataGridViewTextBoxColumn last_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn first_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn middle_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn birthdate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contact_no;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user_level;
+    }
+}
+
---
+++ DGVMerge/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DGVMerge
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection con = new MySqlConnection(@"datasource=localhost;port=3306;username=root;password=admin");
+        public Form1()
+        {
+            InitializeComponent();
+            GetUsers();
+            DGVUsers.Paint += new PaintEventHandler(DGVUsers_Paint);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void GetUsers()
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM dbpos.users",con);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable dtset = new DataTable();
+            adapter.Fill(dtset);
+            BindingSource bsource = new BindingSource();
+            bsource.DataSource = dtset;
+            DGVUsers.DataSource = bsource;
+        }
+
+        private void DGVUsers_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle lnameRect = DGVUsers.GetCellDisplayRectangle(3, -1, true);
+            Rectangle fnameRect = DGVUsers.GetCellDisplayRectangle(4, -1, true);
+            Rectangle mnameRect = DGVUsers.GetCellDisplayRectangle(5, -1, true);
+
+            int combinedWidth = lnameRect.Width + fnameRect.Width + mnameRect.Width;
+
+            Rectangle mergedHeaderRect = new Rectangle(lnameRect.X, lnameRect.Y, combinedWidth, lnameRect.Height);
+
+            e.Graphics.FillRectangle(new SolidBrush(SystemColors.Control), mergedHeaderRect);
+            e.Graphics.DrawRectangle(Pens.LightGray, mergedHeaderRect);
+            e.Graphics.DrawString("FULL NAME", DGVUsers.ColumnHeadersDefaultCellStyle.Font, Brushes.Black, mergedHeaderRect, new StringFormat
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            });
+        }
+    }
+}
